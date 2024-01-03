415d0fd31fe1fac15e899e656c09fdaf8a2906b8
Jose Oleriano <joserobertoadvir@gmail.com>
2024-01-03 16:25:07 -0300

    Add getters e setters

--- app/src/main/java/oleriano/jose/listacurso/view/MainActivity.java
+++ app/src/main/java/oleriano/jose/listacurso/view/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import oleriano.jose.listacurso.R;
+import oleriano.jose.listacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("José Roberto");
+        pessoa.setSobreNome("Oleriano");
+        pessoa.setNomeCurso("Android");
+        pessoa.setTelefoneContato("44-999999999");
     }
 }
