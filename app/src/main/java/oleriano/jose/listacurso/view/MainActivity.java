9bc5459ede160bd10d79a49ddf9696d777f12f54
Jose Oleriano <joserobertoadvir@gmail.com>
2024-01-03 23:52:50 -0300

    Funcionalidades aos botoes

--- app/src/main/java/oleriano/jose/listacurso/view/MainActivity.java
+++ app/src/main/java/oleriano/jose/listacurso/view/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import oleriano.jose.listacurso.R;
 import oleriano.jose.listacurso.model.Pessoa;
@@ -17,23 +21,77 @@ public class MainActivity extends AppCompatActivity {
 
     String dadosOutraPessoa;
 
+    EditText editPrimeiroNome;
+    EditText editSobrenome;
+    EditText editNomeCurso;
+    EditText editTelefoneContato;
+    Button btnLimpar;
+    Button btnSalvar;
+    Button btnFinalizar;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
-        pessoa.setPrimeiroNome("José Roberto");
-        pessoa.setSobreNome("Oleriano");
-        pessoa.setNomeCurso("Android");
-        pessoa.setTelefoneContato("44-999999999");
+        // pessoa.setPrimeiroNome("José Roberto");
+        // pessoa.setSobreNome("Oleriano");
+        // pessoa.setNomeCurso("Android");
+        //pessoa.setTelefoneContato("44-999999999");
 
         outraPessoa = new Pessoa();
         outraPessoa.setPrimeiroNome("Ana Jessica");
         outraPessoa.setSobreNome("Costa");
-        pessoa.setNomeCurso("Android");
-        pessoa.setTelefoneContato("44-999999998");
+        outraPessoa.setNomeCurso("Java");
+        outraPessoa.setTelefoneContato("44-999999998");
 
+        editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
+        editSobrenome = findViewById(R.id.editSobrenome);
+        editNomeCurso = findViewById(R.id.editNomeCurso);
+        editTelefoneContato = findViewById(R.id.editTelefoneContato);
+
+        btnLimpar = findViewById(R.id.btnLimpar);
+        btnSalvar = findViewById(R.id.btnSalvar);
+        btnFinalizar = findViewById(R.id.btnFinalizar);
+
+        editPrimeiroNome.setText(pessoa.getPrimeiroNome());
+        editSobrenome.setText(pessoa.getSobreNome());
+        editNomeCurso.setText(pessoa.getNomeCurso());
+        editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editPrimeiroNome.setText("");
+                editSobrenome.setText("");
+                editNomeCurso.setText("");
+                editTelefoneContato.setText("");
+            }
+        });
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Volte sempre", Toast.LENGTH_LONG).show();
+                fileList();
+            }
+        });
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+                pessoa.setSobreNome(editSobrenome.getText().toString());
+                pessoa.setNomeCurso(editNomeCurso.getText().toString());
+                pessoa.setTelefoneContato(editTelefoneContato.getText().toString());
+
+
+                Toast.makeText(MainActivity.this, "Salvo "+pessoa.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+/*
         dadosPessoa = "Primeiro Nome: ";
         dadosPessoa += pessoa.getPrimeiroNome();
         dadosPessoa += "Sobrenome: ";
@@ -51,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         dadosOutraPessoa += outraPessoa.getNomeCurso();
         dadosOutraPessoa += "Telefone de Contato: ";
         dadosOutraPessoa += outraPessoa.getTelefoneContato();
+*/
 
 
         Log.i("POOAndroid", "Object pessoa: "+ pessoa.toString());
